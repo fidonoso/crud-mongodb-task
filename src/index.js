36b2d2e180a3ccc4282010aee3f84e87bd636512
Fernando Donoso <fidonoso@gmail.com>
2022-05-05 18:41:39 -0400

    gestor de tareas v1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './database'
 import{PORT} from './config'
 
 //babel-node src/index.js
-console.log('ejecutar: babel-node src/index.js')
+console.log('ejecutar: npm run dev')
 app.listen(PORT);
 console.log(`Escuchandoi en el puerto ${PORT}`)
 
